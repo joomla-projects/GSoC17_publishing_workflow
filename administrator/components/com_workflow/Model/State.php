30e1650228e9efca59cfe6509dbba3ec7d6dfb19
Shubham Rajput <shubham.rajput2013@vit.ac.in>
2017-07-24 20:01:33 +0530

    Some of the sql fixing and some improving the code style

--- administrator/components/com_workflow/Model/State.php
+++ administrator/components/com_workflow/Model/State.php
@@ -96,7 +96,7 @@ class State extends Admin
 		}
 
 		$app = \JFactory::getApplication();
-		$extension = $app->getUserStateFromRequest($this->context . '.filter.extension', 'extension', 0, 'cmd');
+		$extension = $app->getUserStateFromRequest($this->context . '.filter.extension', 'extension', 0, 'int');
 
 		$isAssigned = WorkflowHelper::callMethodFromHelper($extension, 'canDeleteState', $record->id);
 
@@ -228,7 +228,7 @@ class State extends Admin
 		$table = $this->getTable();
 		$pks   = (array) $pks;
 		$app = \JFactory::getApplication();
-		$extension = $app->getUserStateFromRequest($this->context . '.filter.extension', 'extension', 0, 'cmd');
+		$extension = $app->getUserStateFromRequest($this->context . '.filter.extension', 'extension', 0, 'int');
 
 		// Default menu item existence checks.
 		if ($value != 1)
