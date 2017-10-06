c41c52cc279536cd6663d579bf16821099e95c50
Buddhima <buddhimawijeweera@yahoo.com>
2017-10-06 21:58:00 +0530

    cleanup sql script

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2017-09-17.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2017-09-17.sql
@@ -26,13 +26,6 @@ CREATE TABLE IF NOT EXISTS `#__workflows` (
 ) ENGINE=InnoDB COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `#__workflows`
---
-
--- INSERT INTO `#__workflows` (`id`, `asset_id`, `published`, `title`, `description`, `extension`, `default`, `created`, `created_by`, `modified`, `modified_by`) VALUES
--- (1, 56, 1, 'Joomla! Default', '', 'com_content', 1, NOW(), 0, '0000-00-00 00:00:00', 0);
-
---
 -- Table structure for table `#__workflow_associations`
 --
 
@@ -67,16 +60,6 @@ CREATE TABLE IF NOT EXISTS `#__workflow_states` (
 ) ENGINE=InnoDB DEFAULT COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `#__workflow_states`
---
-
--- INSERT INTO `#__workflow_states` (`id`, `asset_id`, `workflow_id`, `published`, `title`, `description`, `condition`, `default`) VALUES
--- (1, 57, 1, 1, 'Unpublished', '', '0', 0),
--- (2, 58, 1, 1, 'Published', '', '1', 1),
--- (3, 59, 1, 1, 'Trashed', '', '-2', 0),
--- (4, 60, 1, 1, 'Archived', '', '1', 0);
-
---
 -- Dumping data for table `#__workflow_transitions`
 --
 
@@ -97,53 +80,7 @@ CREATE TABLE IF NOT EXISTS `#__workflow_transitions` (
   KEY `workflow_id` (`workflow_id`)
 ) ENGINE=InnoDB DEFAULT COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `#__workflow_transitions`
---
-
--- INSERT INTO `#__workflow_transitions` (`id`, `asset_id`, `published`, `title`, `description`, `from_state_id`, `to_state_id`, `workflow_id`) VALUES
--- (1, 61, 1, 'Unpublish', '', 2, 1, 1),
--- (2, 62, 1, 'Unpublish', '', 3, 1, 1),
--- (3, 63, 1, 'Unpublish', '', 4, 1, 1),
--- (4, 64, 1, 'Publish', '', 1, 2, 1),
--- (5, 65, 1, 'Publish', '', 3, 2, 1),
--- (6, 66, 1, 'Publish', '', 4, 2, 1),
--- (7, 67, 1, 'Trash', '', 1, 3, 1),
--- (8, 68, 1, 'Trash', '', 2, 3, 1),
--- (9, 69, 1, 'Trash', '', 4, 3, 1),
--- (10, 70, 1, 'Archive', '', 1, 4, 1),
--- (11, 71, 1, 'Archive', '', 2, 4, 1),
--- (12, 72, 1, 'Archive', '', 3, 4, 1);
-
 -- Adds com_workflow entry into the #__extensions table
 
 INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `checked_out`, `checked_out_time`, `ordering`, `state`, `namespace`) VALUES
 (35, 0, 'com_workflow', 'component', 'com_workflow', '', 1, 1, 0, 0, '', '{}', 0, '0000-00-00 00:00:00', 0, 0, 'Joomla\\Component\\Workflow');
-
--- UPDATE `#__categories` SET `params` = '{"category_layout":"","image":"","workflow_id":"1"}' WHERE `extension` = 'com_content';
-
--- INSERT INTO `#__assets` (`id`, `parent_id`, `lft`, `rgt`, `level`, `name`, `title`, `rules`) VALUES
--- (56, 8, 20, 53, 2, 'com_content.workflow.1', 'Joomla! Default', '{}'),
--- (57, 56, 21, 22, 3, 'com_content.state.1', 'Unpublished', '{}'),
--- (58, 56, 23, 24, 3, 'com_content.state.2', 'Published', '{}'),
--- (59, 56, 25, 26, 3, 'com_content.state.3', 'Trashed', '{}'),
--- (60, 56, 27, 28, 3, 'com_content.state.4', 'Archived', '{}'),
--- (61, 56, 29, 30, 3, 'com_content.transition.1', 'Unpublish', '{}'),
--- (62, 56, 31, 32, 3, 'com_content.transition.2', 'Unpublish', '{}'),
--- (63, 56, 33, 34, 3, 'com_content.transition.3', 'Unpublish', '{}'),
--- (64, 56, 35, 36, 3, 'com_content.transition.4', 'Publish', '{}'),
--- (65, 56, 37, 38, 3, 'com_content.transition.5', 'Publish', '{}'),
--- (66, 56, 39, 40, 3, 'com_content.transition.6', 'Publish', '{}'),
--- (67, 56, 41, 42, 3, 'com_content.transition.7', 'Trash', '{}'),
--- (68, 56, 43, 44, 3, 'com_content.transition.8', 'Trash', '{}'),
--- (69, 56, 45, 46, 3, 'com_content.transition.9', 'Trash', '{}'),
--- (70, 56, 47, 48, 3, 'com_content.transition.10', 'Archive', '{}'),
--- (71, 56, 49, 50, 3, 'com_content.transition.11', 'Archive', '{}'),
--- (72, 56, 51, 52, 3, 'com_content.transition.12', 'Archive', '{}');
-
--- UPDATE `#__content` SET `state` = 3 WHERE `state` = -2;
--- UPDATE `#__content` SET `state` = 4 WHERE `state` = 2;
--- UPDATE `#__content` SET `state` = 2 WHERE `state` = 1;
--- UPDATE `#__content` SET `state` = 1 WHERE `state` = 0;
-
-
